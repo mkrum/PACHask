323b6ae364f26d95bf667bba6e21c9c728612b7e
Michael Krumdick <mrkrumdick@gmail.com>
2023-02-28 22:01:36 -0500

    Merging the box code into the better code

--- clean.hs
+++ clean.hs
@@ -25,7 +25,7 @@ errorOf concept dataList =
 generateDataset :: (RandomGen g) => Int -> Rand g x -> Rand g [x]
 generateDataset num valGenerator = sequence (replicate num valGenerator)
 
-pacEvaluate :: PACTuple Float Bool -> Int -> IO Float
+pacEvaluate :: PACTuple x Bool -> Int -> IO Float
 pacEvaluate (valGenerator, generateConcept, learnFn) numTrain = do
 
   hiddenConcept <- evalRandIO (generateConcept)
@@ -53,7 +53,7 @@ learnInterval dataList =
         then isInInterval (Interval (minimum positive_points) (maximum positive_points) )
         else (\x -> False)
 
-randomIntervalFn :: (RandomGen g) => Rand g (Float -> Bool) 
+randomIntervalFn :: Distribution (Float -> Bool) 
 randomIntervalFn = do
     valOne <- getRandom
     valTwo <- getRandom
@@ -61,9 +61,43 @@ randomIntervalFn = do
         then return (isInInterval (Interval valOne valTwo))
         else return (isInInterval (Interval valTwo valOne))
 
+data Point = Point {xValue :: Float, yValue :: Float}
+
+data BoundingBox = BoundingBox { lowerCorner::Point, upperCorner::Point}
+
+randomBoxFn :: Distribution (Point -> Bool)
+randomBoxFn = do 
+    (Point x1 y1) <- randomPoint
+    (Point x2 y2) <- randomPoint 
+    let (xmin, xmax) = if x1 <= x2 then (x1, x2) else (x2, x1)
+    let (ymin, ymax) = if y1 <= y2 then (y1, y2) else (y2, y1)
+    return (isInBox (BoundingBox (Point xmin ymin) (Point xmax ymax)))
+
+randomPoint :: Distribution Point
+randomPoint = do
+    valOne <- getRandom
+    valTwo <- getRandom
+    return (Point valOne valTwo)
+
+isInBox :: BoundingBox -> Point -> Bool
+isInBox (BoundingBox (Point x1 y1) (Point x2 y2)) (Point inputX inputY) = and [(inputX >= x1), (inputX <= x2), (inputY >= y1), (inputY <= y2)]
+
+learnBoundingBox :: [(Point, Bool)] -> Concept Point Bool
+learnBoundingBox dataList =
+      let positive_examples = filter (\x -> snd x) dataList
+          positive_points = map (\x -> fst x) positive_examples
+      in if (length positive_points) > 0
+          then let xValues = map xValue positive_points
+                   yValues = map yValue positive_points
+                   lowerBounds = (Point (minimum xValues) (minimum yValues))
+                   upperBounds = (Point (maximum xValues) (maximum yValues))
+                in (isInBox (BoundingBox lowerBounds upperBounds))
+          else (\x -> False)
+
 main = do
 
   let intervalPAC = (getRandom, randomIntervalFn, learnInterval) :: PACTuple Float Bool
-  val <- pacEvaluate intervalPAC 10
+  let intervalBox = (randomPoint, randomBoxFn, learnBoundingBox) :: PACTuple Point Bool
+  val <- pacEvaluate intervalBox 10
 
   putStrLn (show val)
